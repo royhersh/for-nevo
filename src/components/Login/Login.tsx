27d01022e1f1210eee969a8741357c2eca2b8273
Roy Hershkovitz <roy.hersh@gmail.com>
2023-04-12 16:05:09 +0300

    Login component (#3)
    * chore: install styled-components
    
    * feat: app background
    
    * feat: Login component
    
    ---------
    
    Co-authored-by: Roy Hershkovitz <place-il@Place-iLs-MBP.localdomain>

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+import { ReactComponent as UserIcon } from './assets/userIcon.svg';
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 420px;
+`;
+const Header = styled.div`
+    font-size: 40px;
+    text-align: center;
+    line-height: 1.3;
+    color: black;
+    margin-bottom: 20px;
+`;
+
+const CreateUserButton = styled.button`
+    margin-top: 15px;color: black;
+    font-size: 30px;
+    background: linear-gradient(270deg, rgb(0, 74, 173) 0%, rgb(203, 108, 230) 100%);
+    padding: 0.2em 1.4em
+`;
+
+const Login = () => {
+    console.log('df');
+    return (
+        <Wrapper>
+            <Header>שלום נא ליצור משתמש כדי להשתמש באפליקציה</Header>
+            <UserIcon style={{width: '220px'}}/>
+            <CreateUserButton>צור משתמש</CreateUserButton>
+        </Wrapper>
+        );
+};
+
+export default Login;
