8642252e2d0b8fedcfb871079190c8fb1f2c2bbd
Roy Hershkovitz <place-il@Place-iLs-MBP.localdomain>
2023-04-12 16:04:50 +0300

    feat: Login component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import styled from 'styled-components';
+import Login from './components/Login';
 
 const StyledApp = styled.div`
   width: 100vw;
   height: 100vh;
   background: linear-gradient(90deg, rgb(0, 74, 173) 0%, rgb(203, 108, 230) 100%) ;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const App = () => {
 
   return (
     <StyledApp>
-      
+        <Login />
     </StyledApp>
   )
 }
