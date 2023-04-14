f94cab68026c06108521c9e6f4e27326322493a0
Roy Hershkovitz <roy.hersh@gmail.com>
2023-04-14 16:58:35 +0300

    chore: add folder aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
+import path from 'path';
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, './src'),
+      '@icons': path.resolve(__dirname, './src/assets/icons'),
+    }
+  }
 })
